ce0784cc0f643149881a2d55191fb9b618c2a72f
arto-leskinen <arto.leskinen@tek.fi>
2025-07-15 14:23:39 +0300

    modified:   settings.py

--- registrations/settings.py
+++ registrations/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k%1tav_=b-zdlha-%hia$_86l66&5izxx@a+7+kp0&fm(@0!5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
